2fc79e8a47593de64afd1e4c6ebb4e529471e96c
Ruckin <fredericocazzi@gmail.com>
2024-07-17 17:52:08 -0300

    First functional application example. Frequency calculator for low pass filter.

---
+++ graphicsTool/frequencyCalculator/frequencyCalculator/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace frequencyCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.resValue = new System.Windows.Forms.TextBox();
+            this.capValue = new System.Windows.Forms.TextBox();
+            this.freqValue = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Resistor";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(197, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ohms";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Capacitor";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(194, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "F";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(19, 123);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Frequency";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(194, 123);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(23, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Hz";
+            // 
+            // resValue
+            // 
+            this.resValue.Location = new System.Drawing.Point(91, 13);
+            this.resValue.Name = "resValue";
+            this.resValue.Size = new System.Drawing.Size(100, 22);
+            this.resValue.TabIndex = 6;
+            // 
+            // capValue
+            // 
+            this.capValue.Location = new System.Drawing.Point(91, 42);
+            this.capValue.Name = "capValue";
+            this.capValue.Size = new System.Drawing.Size(100, 22);
+            this.capValue.TabIndex = 7;
+            // 
+            // freqValue
+            // 
+            this.freqValue.Location = new System.Drawing.Point(91, 123);
+            this.freqValue.Name = "freqValue";
+            this.freqValue.Size = new System.Drawing.Size(100, 22);
+            this.freqValue.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(91, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 173);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.freqValue);
+            this.Controls.Add(this.capValue);
+            this.Controls.Add(this.resValue);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Name = "Form1";
+            this.Text = "Frenquecy Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox resValue;
+        private System.Windows.Forms.TextBox capValue;
+        private System.Windows.Forms.TextBox freqValue;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ graphicsTool/frequencyCalculator/frequencyCalculator/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace frequencyCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            float resistor, capacitor, frequency;
+
+            resistor = float.Parse(resValue.Text);
+            capacitor = float.Parse(capValue.Text);
+
+            frequency = 1 / (2 * 3.14159f * resistor * capacitor);
+
+            freqValue.Text = frequency.ToString();
+
+        }
+    }
+}
